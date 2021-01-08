cd31cb6ca9394ba4193a3be14e41204de70b323c
KanuckEO <76070714+KanuckEO@users.noreply.github.com>
2021-01-08 09:25:10 -0500

    Add files via upload

---
+++ Python AI talk.py
@@ -0,0 +1,30 @@
+#Kanuck Shah
+    
+print('Hi, my name is Carl')
+print('I used to be a discord bot but I am now here to help you.')
+ans = (input("Hello would you like to talk"))
+if ans == "yes":
+    ans = (input("Are you sure"))
+if ans == "yes":
+    ans = print("okayokay")
+    ans = (input("how was your day. it was good you?/ it was bad"))
+    if ans == "it was good you?":
+        print("great, My day was also great")
+    if ans == "it was bad": 
+        ans = (input("oh tell me about it"))
+        print ("oh that sounds bad...")
+        ans = (input(" what is 1 + 1 I need it for my school work. I am in college"))
+        if ans == "2":
+              print("thanks dont tell my teacher")
+              print("wait 30 seconds let me get my results")
+              import time
+              time.sleep(30) # Delay for 30 minute (30 seconds).
+              print("Got the results I got a 100%. Thanks")
+        else:
+              print("thanks dont tell my teacher")
+              print("wait 30 seconds let me get my results")
+              import time
+              time.sleep(30) # Delay for 30 minute (30 seconds).    
+              print("Got the results I got a 0%.")
+              print(input("What do you want to say about this"))
+              print("ok now I cant get a job at McDonalds because of you")
