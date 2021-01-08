9d98bb4e0a3c7aa02609b369e9312e388ae54f60
KanuckEO <76070714+KanuckEO@users.noreply.github.com>
2021-01-08 12:44:56 -0500

    Update Python AI talk.py

--- Python AI talk.py
+++ Python AI talk.py
@@ -1,7 +1,8 @@
 #Kanuck Shah
+#AI talking in Python
     
 print('Hi, my name is Carl')
-print('I used to be a discord bot but I am now here to help you.')
+print('I used to be a discord bot but I am now here to talk with you.')
 ans = (input("Hello would you like to talk"))
 if ans == "yes":
     ans = (input("Are you sure"))
@@ -18,13 +19,30 @@ if ans == "yes":
               print("thanks dont tell my teacher")
               print("wait 30 seconds let me get my results")
               import time
-              time.sleep(30) # Delay for 30 minute (30 seconds).
+              time.sleep(30) # Delay for 30 seconds (30 seconds).
               print("Got the results I got a 100%. Thanks")
+              print("Yay! now I can get my dream job at McDonalds because of you, Thanks")
+              print(input("Well life choices aside what time is it there"))
+              from datetime import datetime
+
+              now = datetime.now()
+
+              current_time = now.strftime("%H:%M")
+              print("Cool it is ", current_time)
+
         else:
               print("thanks dont tell my teacher")
               print("wait 30 seconds let me get my results")
               import time
-              time.sleep(30) # Delay for 30 minute (30 seconds).    
+              time.sleep(30) # Delay for 30 seconds (30 seconds).    
               print("Got the results I got a 0%.")
               print(input("What do you want to say about this"))
-              print("ok now I cant get a job at McDonalds because of you")
+              print("ok now I cant get my dream job at McDonalds because of you")
+              print("that is ok I can still get a job at Google")
+              print(input("Well life choices aside what time is it there"))
+              from datetime import datetime
+
+              now = datetime.now()
+
+              current_time = now.strftime("%H:%M")
+              print("Cool it is ", current_time)
