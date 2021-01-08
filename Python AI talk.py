a119afa128f70a4e0fa0dbcb54bf6ffb4014fa01
KanuckEO <76070714+KanuckEO@users.noreply.github.com>
2021-01-08 12:51:17 -0500

    Update Python AI talk.py

--- Python AI talk.py
+++ Python AI talk.py
@@ -11,6 +11,39 @@ if ans == "yes":
     ans = (input("how was your day. it was good you?/ it was bad"))
     if ans == "it was good you?":
         print("great, My day was also great")
+        ans = (input(" what is 1 + 1 I need it for my school work. I am in college"))
+        if ans == "2":
+              print("thanks dont tell my teacher")
+              print("wait 30 seconds let me get my results")
+              import time
+              time.sleep(30) # Delay for 30 seconds (30 seconds).
+              print("Got the results I got a 100%. Thanks")
+              print("Yay! now I can get my dream job at McDonalds because of you, Thanks")
+              print(input("Well life choices aside what time is it there"))
+              from datetime import datetime
+
+              now = datetime.now()
+
+              current_time = now.strftime("%H:%M")
+              print("Cool it is ", current_time)
+
+        else:
+              print("thanks dont tell my teacher")
+              print("wait 30 seconds let me get my results")
+              import time
+              time.sleep(30) # Delay for 30 seconds (30 seconds).    
+              print("Got the results I got a 0%.")
+              print(input("What do you want to say about this"))
+              print("ok now I cant get my dream job at McDonalds because of you")
+              print("that is ok I can still get a job at Google")
+              print(input("Well life choices aside what time is it there"))
+              from datetime import datetime
+
+              now = datetime.now()
+
+              current_time = now.strftime("%H:%M")
+              print("Cool it is ", current_time)
+              print("Dummy")
     if ans == "it was bad": 
         ans = (input("oh tell me about it"))
         print ("oh that sounds bad...")
@@ -46,3 +79,5 @@ if ans == "yes":
 
               current_time = now.strftime("%H:%M")
               print("Cool it is ", current_time)
+              print("Dummy")
+
